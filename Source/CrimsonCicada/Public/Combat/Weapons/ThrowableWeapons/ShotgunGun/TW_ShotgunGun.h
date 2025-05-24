6e77b089ff807700d83286b77206185fef6ca16b
Marko Petric <markop20042004@gmail.com>
2025-05-24 16:22:07 +0200

    More shotgun gun setup

--- Source/CrimsonCicada/Public/Combat/Weapons/ThrowableWeapons/ShotgunGun/TW_ShotgunGun.h
+++ Source/CrimsonCicada/Public/Combat/Weapons/ThrowableWeapons/ShotgunGun/TW_ShotgunGun.h
@@ -3,27 +3,21 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Combat/Weapons/RangedWeapons/RangedWeaponsBase.h"
-#include "RW_ShotgunGun.generated.h"
+#include "Combat/Weapons/ThrowableWeapons/ThrowableWeaponsBase.h"
+#include "TW_ShotgunGun.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class CRIMSONCICADA_API ARW_ShotgunGun : public ARangedWeaponsBase
+class CRIMSONCICADA_API ATW_ShotgunGun : public AThrowableWeaponsBase
 {
 	GENERATED_BODY()
 
 protected:
-	void BeginPlay() override;
+	virtual void BeginPlay() override;
 	
 	virtual EWeapon GetWeaponEnum() const override { return EWeapon::ShotgunGun; }
 	
 	virtual void PerformPrimaryAction() override;
-
-private:
-	float Range{ 2000.0f };
-	float SpreadRange{ 300.0f };
-
-	USceneComponent* PelletShootSceneComponent;
 };
