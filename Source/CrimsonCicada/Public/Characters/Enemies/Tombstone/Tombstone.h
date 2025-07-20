833561597a8c56ab83c61dca196d413bad97ca4a
Rxamz <n.arsovic06@gmail.com>
2025-07-20 14:21:56 +0200

    Basic tombstone
    Basic Tombstone stuff so we can implement damage

--- Source/CrimsonCicada/Public/Characters/Enemies/Tombstone/Tombstone.h
+++ Source/CrimsonCicada/Public/Characters/Enemies/Tombstone/Tombstone.h
@@ -6,15 +6,31 @@
 #include "Characters/Enemies/BaseEnemyCharacter.h"
 #include "Tombstone.generated.h"
 
-/**
- * 
- */
+UENUM(BlueprintType)
+enum  class ETombstoneState : uint8
+{
+	Idling,
+	Chasing,
+	Attacking,
+	
+};
 UCLASS()
 class CRIMSONCICADA_API ATombstone : public ABaseEnemyCharacter
 {
 	GENERATED_BODY()
 public:
 	ATombstone();
-	
-	
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+	virtual UBehaviorTree* GetBehaviorTree() const override;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Tombstone")
+	UAnimMontage* SwingMontage;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tombstone")
+	float SwingRange;
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadWrite, Category = "Tombstone")
+	ETombstoneState TombstoneState = ETombstoneState::Idling;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Tombstone")
+	bool bIsAttacking = false;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Tombstone")
+	bool bTargetTooClose = false;
 };
